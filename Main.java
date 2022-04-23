4c6f9ccc08cf0eeab9b0358ec3340664f3787ea8
mbalcik <79659698+mbalcik@users.noreply.github.com>
2022-04-23 03:33:18 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ import java.util.concurrent.ThreadLocalRandom;
 
 import game.*;
 
+// Koç University lab assignment
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -35,7 +37,6 @@ public class Main {
 			int p1guess= scanner.nextInt();
 			player11.setGuess(p1guess);
 			
-			//inlab for player1
 			player11.setScore(Math.abs(random-p1guess+player11.getScore()));
 			
 			
@@ -44,20 +45,17 @@ public class Main {
 			int p2guess= scanner.nextInt();
 			player22.setGuess(p2guess);
 			
-			//inlab for player2
 			player22.setScore(Math.abs(random-p2guess+player22.getScore()));
 			
 			System.out.println("The number was "+ random +".");
 			game.play(random);
 			
 			if(starter==tours) {
-				game.declareWinner();
 				
-	
 				System.out.println("Game Result: " +player11.getPlayerName() +": " + player11.getNumberOfWins() +" "+ player22.getPlayerName() +": " + player22.getNumberOfWins());
 				System.out.println("Game Scores: " +player11.getPlayerName() +": " + player11.getScore()+" "+ player22.getPlayerName() +": " + player22.getScore());
-				
-				
+				System.out.println("Closest to 0 wins");
+				game.declareWinner();
 			}
 			starter++;
 		}
